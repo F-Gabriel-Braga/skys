3a9b7201eb6d77c175288ce06fcec0371913e151
f-gabriel-braga <gabrielbraga0712@gmail.com>
2023-05-18 13:34:57 -0300

    create modal ticket

--- src/screens/Tickets/index.jsx
+++ src/screens/Tickets/index.jsx
@@ -1,17 +1,24 @@
-import { Button, Table } from "react-bootstrap";
+import { Button, Modal, Table } from "react-bootstrap";
 import "./style.css";
+import { useState } from "react";
 
 export default function Tickets() {
+
+    const [showTicket, setShowTicket] = useState(false);
+
+    const handleCloseTicket = () => setShowTicket(false);
+    const handleShowTicket = () => setShowTicket(true);
+
     return (
         <div className="tickets sec">
             <h2>Reservas</h2>
             <Table striped bordered hover>
                 <colgroup>
-                    <col style={{width: "15%"}} />
-                    <col style={{width: "25%"}} />
-                    <col style={{width: "25%"}} />
-                    <col style={{width: "15%"}} />
-                    <col style={{width: "20%"}} />
+                    <col style={{ width: "15%" }} />
+                    <col style={{ width: "25%" }} />
+                    <col style={{ width: "25%" }} />
+                    <col style={{ width: "15%" }} />
+                    <col style={{ width: "20%" }} />
                 </colgroup>
                 <thead>
                     <tr>
@@ -58,11 +65,11 @@ export default function Tickets() {
             <h2>Viagens</h2>
             <Table striped bordered hover>
                 <colgroup>
-                    <col style={{width: "15%"}} />
-                    <col style={{width: "25%"}} />
-                    <col style={{width: "25%"}} />
-                    <col style={{width: "20%"}} />
-                    <col style={{width: "15%"}} />
+                    <col style={{ width: "15%" }} />
+                    <col style={{ width: "25%" }} />
+                    <col style={{ width: "25%" }} />
+                    <col style={{ width: "20%" }} />
+                    <col style={{ width: "15%" }} />
                 </colgroup>
                 <thead>
                     <tr>
@@ -80,7 +87,7 @@ export default function Tickets() {
                         <td>São Paulo, SP</td>
                         <td>20/6/2023</td>
                         <td className="d-flex flex-row gap-2 justify-content-center">
-                            <Button>Bilhete</Button>
+                            <Button onClick={handleShowTicket}>Bilhete</Button>
                         </td>
                     </tr>
                     <tr>
@@ -89,7 +96,7 @@ export default function Tickets() {
                         <td>São Paulo, SP</td>
                         <td>20/6/2023</td>
                         <td className="d-flex flex-row gap-2 justify-content-center">
-                            <Button>Bilhete</Button>
+                            <Button onClick={handleShowTicket}>Bilhete</Button>
                         </td>
                     </tr>
                     <tr>
@@ -98,11 +105,24 @@ export default function Tickets() {
                         <td>São Paulo, SP</td>
                         <td>20/6/2023</td>
                         <td className="d-flex flex-row gap-2 justify-content-center">
-                            <Button>Bilhete</Button>
+                            <Button onClick={handleShowTicket}>Bilhete</Button>
                         </td>
                     </tr>
                 </tbody>
             </Table><br />
+
+            <Modal show={showTicket} onHide={handleCloseTicket}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Fortaleza <i className="bi bi-arrow-right"></i> São Paulo</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <span>N° 123</span>
+                    <span>Fortaleza, CE <i className="bi bi-arrow-right"></i> São Paulo, SP</span>
+                    <span>20/6/2023</span>
+                    <span>02:35 - 7:35</span>
+                    <span className="pay">Pagamento realizado com sucesso</span>
+                </Modal.Body>
+            </Modal>
         </div>
     );
 }
