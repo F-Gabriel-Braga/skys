cc748b4cd19bc7c73a56c692f2ec7fe17e3e4048
f-gabriel-braga <gabrielbraga0712@gmail.com>
2023-05-23 23:03:13 -0300

    create search flights

--- src/screens/Flights/index.jsx
+++ src/screens/Flights/index.jsx
@@ -1,77 +1,47 @@
 import { Button, Modal, Table } from "react-bootstrap";
 import "./style.css";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Ticket from "../../components/Ticket";
+import AuthContext from "../../context/AuthContext";
+import axios from "axios";
+import config from "../../config/api-config";
+import { useSearchParams } from "react-router-dom";
 
 export default function Flights() {
 
-    const [showConfTicket, setShowConfTicket] = useState(false);
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [params, setParams] = useState(null);
+    const { userLogged } = useContext(AuthContext);
+    const [flights, setFlights] = useState([]);
 
-    const handleCloseConfTicket = () => setShowConfTicket(false);
-    const handleShowConfTicket = () => setShowConfTicket(true);
+    useEffect(() => {
+        const queryParams = {
+            from: searchParams.get("from"),
+            to: searchParams.get("to"),
+            dateHourFlight: searchParams.get("dateHourFlight")
+        };
+        setParams(queryParams);
+    }, []);
 
-    const tickets = [
-        {
-            id: 1,
-            situation: "IDA",
-            date: "22/7/2023",
-            hour: "12:00",
-            from: "Fortaleza, CE",
-            to: "São Paulo, SP",
-            price: 1200
-        },
-        {
-            id: 1,
-            situation: "IDA",
-            date: "22/7/2023",
-            hour: "12:00",
-            from: "Fortaleza, CE",
-            to: "São Paulo, SP",
-            price: 1200
-        },
-        {
-            id: 1,
-            situation: "IDA",
-            date: "22/7/2023",
-            hour: "12:00",
-            from: "Fortaleza, CE",
-            to: "São Paulo, SP",
-            price: 1200
-        },
-        {
-            id: 1,
-            situation: "IDA",
-            date: "22/7/2023",
-            hour: "12:00",
-            from: "Fortaleza, CE",
-            to: "São Paulo, SP",
-            price: 1200
-        },
-        {
-            id: 1,
-            situation: "IDA",
-            date: "22/7/2023",
-            hour: "12:00",
-            from: "Fortaleza, CE",
-            to: "São Paulo, SP",
-            price: 1200
-        },
-        {
-            id: 1,
-            situation: "IDA",
-            date: "22/7/2023",
-            hour: "12:00",
-            from: "Fortaleza, CE",
-            to: "São Paulo, SP",
-            price: 1200
+    useEffect(() => {
+        if(userLogged && params) {
+            const headers = { "Authorization": `${userLogged.tokenType} ${userLogged.accessToken}` };
+            console.log(headers)
+            axios.post(`${config.BASE_URL}/flights/filter`, params, { headers }).then(response => {
+                setFlights(response.data);
+            }).catch(error => {
+                console.log(error);
+            })
         }
-    ]
+    }, [userLogged, params]);
 
     return (
-
         <div className="flitghs sec">
             <h2>Voos</h2>
-            {tickets.map(ticket => <Ticket {...ticket} />)}
+            {flights.length == 0 ?
+                <span className="search-empty">Nehum voo encontrado.</span> : 
+                flights.map(flight => <Ticket {...flight} />)
+            }
         </div>
     );
 }
--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -4,10 +4,14 @@ import { Button } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import config from "../../config/api-config";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 export default function Home() {
 
     const [cities, setCities] = useState([]);
+    const navigate = useNavigate();
+    const { handleSubmit, register, formState: { errors } } = useForm();
 
     useEffect(() => {
         axios.get(config.IBGE_CITIES).then(response => {
@@ -17,29 +21,34 @@ export default function Home() {
         });
     }, []);
 
+    function onSubmit(data) {
+        const { dateHourFlight, from, to } = data;
+        navigate(`/flights?from=${from}&to=${to}&dateHourFlight=${dateHourFlight}`);
+    }
+
     return (
         <div className="home" style={{ backgroundImage: `url(${background})` }}>
             {/* <a href="https://br.freepik.com/fotos-gratis/vista-deslumbrante-da-praia-e-do-mar-sob-o-ceu-azul-capturado-em-mombasa-quenia_9076682.htm#query=Fortaleza&position=47&from_view=search&track=sph">Imagem de wirestock</a> no Freepik */}
             <div className="filter-flight">
                 <h3>Busque pôr passagens aéreas</h3>
-                <form className="filters">
-                    <label>
+                <form className="filters" onSubmit={handleSubmit(onSubmit)}>
+                    <label className={errors.from && "field-invalid"}>
                         <i className="bi bi-search me-2"></i> Origem <br />
-                        <input type="text" placeholder="Origem" list="cities" />
+                        <input type="text" placeholder="Origem" list="cities" {...register("from", { required: true })} />
                     </label>
-                    <label>
+                    <label className={errors.to && "field-invalid"}>
                         <i className="bi bi-search me-2"></i> Destino <br />
-                        <input type="text" placeholder="Destino" list="cities" />
+                        <input type="text" placeholder="Destino" list="cities" {...register("to", { required: true })} />
                     </label>
-                    <label>
+                    <label className={errors.dateHourFlight && "field-invalid"}>
                         <i className="bi bi-calendar-week me-2"></i> Data <br />
-                        <input type="date" placeholder="Data" />
+                        <input type="date" placeholder="Data" {...register("dateHourFlight", { required: true })} />
                     </label>
                     <label>
                         <i className="bi bi-arrow-down-up me-2"></i> Situação <br />
                         <input type="text" placeholder="Situação" value="IDA" disabled />
                     </label>
-                    <Button variant="success">Buscar</Button>
+                    <Button variant="success" type="submit">Buscar</Button>
                     <datalist id="cities">
                         {cities.map(city => <option key={city["municipio-id"]} value={`${city["municipio-nome"]}, ${city["UF-sigla"]}`} />)}
                     </datalist>
